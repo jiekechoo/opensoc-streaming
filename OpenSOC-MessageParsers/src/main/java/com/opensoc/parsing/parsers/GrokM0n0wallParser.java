f4746d1dc6142cc5845db2b5f89a48af3dc97d57
jiekechoo <jiekechoo@gmail.com>
2015-07-21 21:13:52 +0800

    fix m0n0wall grok

--- OpenSOC-MessageParsers/src/main/java/com/opensoc/parsing/parsers/GrokM0n0wallParser.java
+++ OpenSOC-MessageParsers/src/main/java/com/opensoc/parsing/parsers/GrokM0n0wallParser.java
@@ -1,59 +1,79 @@
 package com.opensoc.parsing.parsers;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
 import oi.thekraken.grok.api.Match;
 import oi.thekraken.grok.api.Grok;
 import oi.thekraken.grok.api.exception.GrokException;
 
+import org.apache.commons.io.IOUtils;
 import org.json.simple.JSONObject;
 
+public class GrokM0n0wallParser extends AbstractParser {
 
-public class GrokM0n0wallParser extends AbstractParser{
-	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private transient InputStream pattern_url;
 	Grok grok;
-	
-	public GrokM0n0wallParser() throws GrokException
-	{
-		URL pattern_url = getClass().getClassLoader().getResource(
-				"pattarns/m0n0wall");
-		grok = Grok.create(pattern_url.getFile());
-		grok.compile("%{M0N0WALL}");
+
+	public static final String PREFIX = "stream2file";
+	public static final String SUFFIX = ".tmp";
+
+	public static File stream2file(InputStream in) throws IOException {
+		final File tempFile = File.createTempFile(PREFIX, SUFFIX);
+		tempFile.deleteOnExit();
+		try (FileOutputStream out = new FileOutputStream(tempFile)) {
+			IOUtils.copy(in, out);
+		}
+		return tempFile;
 	}
 
-	public GrokM0n0wallParser(String filepath) throws GrokException
-	{
+	public GrokM0n0wallParser() throws GrokException, IOException {
+		// URL pattern_url = getClass().getClassLoader().getResource(
+		// "pattarns/m0n0wall");
+		// grok = Grok.create(pattern_url.getFile());
+		// grok.compile("%{M0N0WALL}");
+
+		pattern_url = getClass().getClassLoader().getResourceAsStream("patterns/m0n0wall");
+
+		File file = stream2file(pattern_url);
+		grok = Grok.create(file.getPath());
+		grok.compile("%{M0N0WALL}");
+
+	}
+
+	public GrokM0n0wallParser(String filepath) throws GrokException {
 
 		grok = Grok.create(filepath);
 		grok.compile("%{M0N0WALL}");
 	}
-	
-	public GrokM0n0wallParser(String filepath, String pattern) throws GrokException
-	{
+
+	public GrokM0n0wallParser(String filepath, String pattern) throws GrokException {
 
 		grok = Grok.create(filepath);
-		grok.compile("%{"+pattern+"}");
+		grok.compile("%{" + pattern + "}");
 	}
-	
+
 	@Override
 	public JSONObject parse(byte[] raw_message) {
 		JSONObject payload = new JSONObject();
 		String toParse = "";
 		JSONObject toReturn;
-		
 
 		try {
 
 			toParse = new String(raw_message, "UTF-8");
 			Match gm = grok.match(toParse);
 			gm.captures();
-			
+
 			toReturn = new JSONObject();
-			
+
 			toReturn.putAll(gm.toMap());
 			toReturn.remove("M0N0WALL");
 			String proto = toReturn.get("protocol").toString();
@@ -61,16 +81,12 @@ public class GrokM0n0wallParser extends AbstractParser{
 			proto = proto.replace("}", "");
 			toReturn.put("protocol", proto);
 			return toReturn;
-			
-		}
-		catch(Exception e)
-		{
+
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
-		
-	}
 
-	
+	}
 
 }
